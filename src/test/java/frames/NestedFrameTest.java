ff10f7342960fb3a4b2da80131501550b0c3638b
Dmitry T <dtolstik@gmail.com>
2021-05-10 19:16:55 -0600

    FramesPage class code changed, NestedFrameTest class code refactored.

--- src/test/java/frames/NestedFrameTest.java
+++ src/test/java/frames/NestedFrameTest.java
@@ -4,25 +4,23 @@ import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FramesPage;
+import static org.testng.Assert.*;
 
 public class NestedFrameTest extends BaseTest {
 
     @Test
     public void testMiddleFrameText() {
-        //FramesPage.NestedFramesPage nestedFramesPage = homePage.clickFramesPageLink().clickNestedFramesLink();
         FramesPage framesPage = homePage.clickFramesPageLink();
         FramesPage.NestedFramesPage nestedFramesPage = framesPage.clickNestedFramesLink();
 
-        Assert.assertEquals(nestedFramesPage.switchToMiddleFrame(), "MIDDLE", "Wrong text");
-        //Assert.assertEquals(nestedFramesPage.getMiddleFrameText(), "MIDDLE", "Wrong middle frame text");
+        assertEquals(nestedFramesPage.getMiddleFrameText(), "MIDDLE", "Wrong middle frame text");
     }
 
-//    @Test
-//    public void testBottomFrameText() {
-//        FramesPage.NestedFramesPage nestedFramesPage = homePage.clickFramesPageLink().clickNestedFramesLink();
-//
-//        nestedFramesPage.switchToBottomFrame();
-//        Assert.assertEquals(nestedFramesPage.getBottomFrameText(), "BOTTOM", "Wrong bottom frame text");
-//    }
+    @Test
+    public void testLowerFrameText() {
+        FramesPage framesPage = homePage.clickFramesPageLink();
+        FramesPage.NestedFramesPage nestedFramesPage = framesPage.clickNestedFramesLink();
 
+        assertEquals(nestedFramesPage.getLowerFrameText(), "BOTTOM", "Wrong lower frame text");
+    }
 }
